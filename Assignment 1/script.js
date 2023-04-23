aea0b52ae04671e09ceeb3ad702c58307c1f8edf
Divyaramphul14 <128127199+Divyaramphul14@users.noreply.github.com>
2023-04-23 08:51:35 +0400

    Add files via upload

---
+++ Assignment 1/script.js
@@ -0,0 +1,16 @@
+//The code was referenced from w3school so as to add the read more button
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
